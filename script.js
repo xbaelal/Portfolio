64c15bbf12fcf93493c3dfdd81c067cb4b78cab4
Bilal <memonbilal760@gmail.com>
2024-07-20 21:20:26 +0500

    add an animation in project-discussion div

--- script.js
+++ script.js
@@ -213,3 +213,33 @@ for (const skill of skills) {
   
     burgerMenuContent.classList.toggle("hidden");
   });
+
+  (function() {
+    let elements;
+    let windowHeight;
+  
+    function init() {
+      elements = document.querySelectorAll('.not-roll');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+        let positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add('roll-in');
+          element.classList.remove('not-roll');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition();
+  })();
+  
+  
